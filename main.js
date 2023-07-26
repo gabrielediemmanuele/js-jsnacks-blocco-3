616ca7e1d995a522d0659803ad395d80ac2ba7f6
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-26 16:50:18 +0200

    feat: esercizio 3 pt.2

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ Verificare che le due parole abbiano la stessa lunghezza.
 Se hanno la stessa lunghezza, stamparle entrambe altrimenti 
 stampare la più lunga delle due.*/
 
-const firstWord = prompt("Inserisci una parola");
-const secondWord = prompt("inserisci un'altra parola");
+/* const firstWord = prompt("Inserisci una parola");
+const secondWord = prompt("inserisci un'altra parola"); */
 
 /* let perfectWord; */
 
@@ -39,7 +39,8 @@ const secondWord = prompt("inserisci un'altra parola");
   console.log(firstWord);
 } */
 
-/! * Soluzione pulita ?*/;
+/* const firstWord = prompt("Inserisci una parola");
+const secondWord = prompt("inserisci un'altra parola");
 
 if (firstWord.length == secondWord.length) {
   console.log(
@@ -49,4 +50,38 @@ if (firstWord.length == secondWord.length) {
   console.log(secondWord);
 } else {
   console.log(firstWord);
+} */
+
+//ESERCIZIO 3
+
+/* Crea un array vuoto e chiedi all'utente 
+un numero da inserire nell'array. 
+Continua a chiedere i numeri all'utente e
+ a inserirli nell'array fino a quando la 
+ somma degli elementi è minore di 50.*/
+
+/* Creo un array vuoto */
+const emptyArray = [];
+
+/* chiedo all'utente un numero da inserire nell'array */
+/* const userNumber = parsInt(prompt("Inserisci qui un numero")); */
+
+/* variabile per contenere la somma*/
+let sum = 0;
+/* continuo a ripetere il prompt */
+while (sum < 50) {
+  let userNumber = parseInt(prompt("Inserisci qui un numero"));
+  sum += userNumber;
+
+  if (sum < 50) {
+    emptyArray.push(userNumber);
+  }
+
+  for (let sum = 0; sum < emptyArray.length; sum++) {
+    sum += emptyArray[sum];
+  }
 }
+
+console.log(emptyArray);
+console.log(sum);
+/* inserire i numeri nell'array fino a quando la somma è minore di 50 */
