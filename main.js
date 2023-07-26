791d76eb513fe7d1dcba3b80b3127193e21d7b5d
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-26 16:02:23 +0200

    feat: add second ex solution

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 // Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
-const firstNumber = prompt("Digita il primo numero !");
+/* const firstNumber = parseInt(prompt("Digita il primo numero !"));
 
-const secondNumber = prompt("Digita il secondo numero !");
+const secondNumber = parseInt(prompt("Digita il secondo numero !"));
 
+let max = firstNumber;
+let min = secondNumber; */
 /* let generatedNumber; */
 
-const genNumber = Math.floor(Math.random() * firstNumber + secondNumber);
+/* const genNumber = Math.floor(Math.random() * (max - min)) + min; */
 
-console.log(genNumber);
+/* const genNumber = Math.floor(Math.random() * firstNumber + secondNumber) + 1; */
+
+/* console.log(genNumber); */
+
+// Correzzione
+
+/* ------------------------------------------------------ */
+
+//ESERCIZIO 2
+
+/* Dare la possibilità di inserire due parole. 
+Verificare che le due parole abbiano la stessa lunghezza. 
+Se hanno la stessa lunghezza, stamparle entrambe altrimenti 
+stampare la più lunga delle due.*/
+
+const firstWord = prompt("Inserisci una parola");
+const secondWord = prompt("inserisci un'altra parola");
+
+let perfectWord;
+
+if (firstWord.length == secondWord.length) {
+  console.log("Le parole hanno la stessa lunghezza");
+} else if (firstWord.length < secondWord.length) {
+  console.log(secondWord);
+} else if (firstWord.length > secondWord.length) {
+  console.log(firstWord);
+}
